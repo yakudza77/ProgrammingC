f0974b0f454197a6eefd5a770999f2b13253f394
Constantin Ovseannicov <ovseannicov.constantin@gmail.com>
2021-04-10 14:12:14 +0300

    merge operation

--- CurrentProjectTest2/CurrentProjectTest2.c
+++ CurrentProjectTest2/CurrentProjectTest2.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
-#define IN 1 /*внутри слова*/
-#define OUT 0 /*вне слова*/
-
-main()
+main() 
 {
-	int  c, nw, state;
-	state = OUT;
-	nw = 0;
-	while ((c = getchar()) != EOF) {
 	
-		if (c == ' ' || c == '\n' || c == '\t') 
-			state = OUT; 
-		else if (state == OUT) { 
-			++nw;
-		}
+	printf("[%c]", "hello");
+	getchar();
+	
 
-	}
-	printf(" word:%d \n", nw); 
 }
