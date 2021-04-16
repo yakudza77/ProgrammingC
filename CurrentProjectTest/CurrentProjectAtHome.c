6d3862899935649f7aef458c58268d976ec869f8
Constantin Ovseannicov <ovseannicov.constantin@gmail.com>
2021-04-16 09:57:30 +0300

    test

--- CurrentProjectTest/CurrentProjectAtHome.c
+++ CurrentProjectTest/CurrentProjectAtHome.c
@@ -1,21 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-main()
-// HOMEWORK 1.13 Напишите программу, печатающую гистограммы длин вводимых слов
-{
-	int c ,nc;
-	nc = 0;
-	printf("Type text:");
-	while ((c = getchar()) != EOF) {
-		++nc;
-		
-		if (c == ' ') {
-			printf("\n");
-		}
-		else {
-			printf("#");
-		}
+int SumDigits(int N)  // заголовок функции
+{                     //начало функции
+	int  sum = 0;
+	while (N != 0)
+	{
+		   
+		sum += N % 10;
+		N = N / 10;
 	}
+	return sum;        //функция возвращает значение sum
+}                      //конец функции
 
-	
-
+main()
+{
+	int N, s;
+	printf("\n Please enter an integer:");//Введите целое число
+	scanf("%d", &N);
+	s = SumDigits(N); //Вызов функции
+	printf("sum of digits of a number %d is equal to %d", N, s);
+	getchar();
+	getchar();
 }
--- CurrentProjectTest2/CurrentProjectTest2.c
+++ CurrentProjectTest2/CurrentProjectTest2.c
@@ -1,9 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-main() 
+main()
 {
-	
-	printf("[%c]", "hello");
-	getchar();
-	
+	float S, z, c, d, a;
+	S = 0; z = 1; c = 1; d = 2; //начальные значения
+	a = 0.5;                    //первый элемент последовательности
 
+	while (a >= 0.001)
+	{
+		S = S + z * a;          //добавить элемент к сумме
+		z = -z;                 //изменить переменные z, c, d
+		c++;
+		d = d * 2;
+		a = c / d;              //модуль следующего элементы
+		printf("\n a = %f", a);
+		printf("\ncurrent sum  S = %f", S);
+	}
+	printf("\nsum S = %f", S);
+
+	getchar();
+	getchar();
 }
