f7252ee4835f07406e53f52c42dc9a56396a9457
Constantin Ovseannicov <ovseannicov.constantin@gmail.com>
2021-10-27 06:20:13 +0300

    Update for programm Geo2Json Add check for input errors Add fprintf error stream

--- Geo2Json/geo2json.c
+++ Geo2Json/geo2json.c
@@ -14,6 +14,14 @@ int main()
 			printf(",\n");
 		else
 			started = 1;
+		if ((latitude < -90) || (latitude > 90)) {
+			fprintf(stderr, "Invalid latitude:%f\n", latitude);
+			return 2;
+		}
+		if ((longitude < -180) || (longitude > 180)) {
+			fprintf(stderr, "Invalid longitude:%f\n", longitude);
+			return 2;
+		}
 		printf("{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
 	}
 	puts("\n]");
@@ -24,3 +32,5 @@ int main()
 //Делаем release exe файл. Создаём файл gpsdata.csv с координатами в формате csv кидаем в ту же папку где программа
 //C помощью cmd находим путь к файлу и делаем C:\Programmtest>018Geo2Json.exe<gpsdata.csv>output.json
 //В результат stdin будет файл gpsdata.csv а stdout будет output.json
+//Добавили проверку ввода данных
+//Добавили функцию fprintf для вывода ошибок в отдельный поток
