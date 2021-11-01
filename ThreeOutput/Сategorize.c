2d4f91710458966b6cb0529dd4e18c66a894cc11
Constantin Ovseannicov <ovseannicov.constantin@gmail.com>
2021-11-01 06:11:31 +0200

    mod programm categorize

--- ThreeOutput/Сategorize.c
+++ ThreeOutput/Сategorize.c
@@ -6,20 +6,20 @@
 int main()
 {
 	char line[80];
-	FILE* in =    fopen("C:/Users/o.kostea/Desktop/Programmtest/spooky.csv", "r");
-	FILE* file1 = fopen("C:/Users/o.kostea/Desktop/Programmtest/ufos.csv", "w");
-	FILE* file2 = fopen("C:/Users/o.kostea/Desktop/Programmtest/disappearances.csv", "w");
-	FILE* file3 = fopen("C:/Users/o.kostea/Desktop/Programmtest/others.csv", "w");
+	FILE* in = fopen("D:/TESTPROGRAMM/spooky.csv", "r");//r читает из файла spooky.csv использу€ поток in
+	FILE* file1 = fopen("D:/TESTPROGRAMM/ufos.csv", "w");//w записывает в файл ufos.csv использу€ поток file1
+	FILE* file2 = fopen("D:/TESTPROGRAMM/disappearances.csv", "w");//w записвает в файл disappearances.csv использу€ поток file2
+	FILE* file3 = fopen("D:/TESTPROGRAMM/others.csv", "w");//w записывает в файл others.csv использу€ поток file3
 
-	while (fscanf(in, "%79[^\n]\n", line) == 1) {
-		if (strstr(line, "UFO"))
-			fprintf(file1, "%s\n", line);
-		else if (strstr(line, "Disappearance"))
-			fprintf(file2, "%s\n", line);
+	while (fscanf(in, "%79[^\n]\n", line) == 1) { //считывает построчно в поток in и отправл€ет данные в массив line 
+		if (strstr(line, "UFO"))              // ищет совпадение в строке массива line cо словом UFO
+			fprintf(file1, "%s\n", line); //если true отправл€ет данную строку line в поток file1 
+		else if (strstr(line, "Disappearance")) // ищет совпадение в строке массива line cо словом Disappearance
+			fprintf(file2, "%s\n", line);  //если true отправл€ет данную строку line в поток file2
 		else
-			fprintf(file3, "%s\n", line);
+			fprintf(file3, "%s\n", line); // всЄ остальное что не совпало отправл€ет line в поток file3
 	}
-	fclose(file1);
+	fclose(file1);// закрываем потоки после завршени€ работы цикла while
 	fclose(file2);
 	fclose(file3);
 	return 0;
